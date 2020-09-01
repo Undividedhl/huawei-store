4feabd35e49d9893d80710da4365c762453319ee
coriander <1019958984@qq.com>
2020-09-01 22:49:39 +0800

    产品分配CRUD

--- huawei-admin/src/main/java/com/suckmydisk/huawei/controller/product/ProductCategoryController.java
+++ huawei-admin/src/main/java/com/suckmydisk/huawei/controller/product/ProductCategoryController.java
@@ -1,14 +1,14 @@
 package com.suckmydisk.huawei.controller.product;
 
-import com.github.pagehelper.PageInfo;
 import com.suckmydisk.huawei.common.core.controller.BaseController;
 import com.suckmydisk.huawei.common.core.domain.AjaxResult;
 import com.suckmydisk.huawei.common.core.page.PageDataInfo;
+import com.suckmydisk.huawei.common.exception.BaseException;
 import com.suckmydisk.huawei.entity.ProductCategory;
 import com.suckmydisk.huawei.service.ProductCategoryService;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -28,13 +28,13 @@ public class ProductCategoryController extends BaseController {
     private ProductCategoryService productCategoryService;
 
     /**
-     * 通过主键查询单条数据
+     * 查询所有数据
      *
-     * @return 产品分类分页数据
+     * @return 分页数据
      */
-    @GetMapping("/productCategories/{id}")
-    @ApiOperation("分页获取产品分类")
-    public AjaxResult queryAll() {
+    @GetMapping("/productCategories")
+    @ApiOperation("获取所有产品分类")
+    public AjaxResult getProductCategory() {
         startPage();
         List<ProductCategory> productCategories = productCategoryService.queryAll();
         PageDataInfo pageDataInfo = new PageDataInfo(productCategories.size(), productCategories);
@@ -42,5 +42,52 @@ public class ProductCategoryController extends BaseController {
         return AjaxResult.success(pageDataInfo);
     }
 
+    /**
+     * 根据ID获取单条数据
+     * 
+     * @param id
+     * @return 单条数据
+     */
+    @GetMapping("/productCategories")
+    public AjaxResult getProductCategoryById(@PathVariable("id") long id) {
+        productCategoryService.queryById(id);
+        return AjaxResult.success(id);
+    }
 
+    /**
+     * 新增一条数据
+     * 
+     * @param productCategory
+     */
+    @PostMapping("/productCategories")
+    public AjaxResult addProductCategory(@RequestParam @Validated ProductCategory productCategory) {
+        productCategoryService.insert(productCategory);
+        return AjaxResult.success();
+    }
+
+    /**
+     * 根据ID删除数据
+     * 
+     * @param id
+     */
+    @DeleteMapping("/productCategories/{id}")
+    public AjaxResult deleteProductCategory(@PathVariable("id") long id) {
+        productCategoryService.deleteById(id);
+        return AjaxResult.success();
+    }
+
+    /**
+     * 更新数据
+     * 
+     * @param productCategory
+     */
+    @PutMapping("/productCategories")
+    public AjaxResult updateProductCategory(@RequestBody @Validated ProductCategory productCategory) {
+        Long id = productCategory.getId();
+        if (id == null) {
+            throw new BaseException("参数为空");
+        }
+        productCategoryService.update(productCategory);
+        return AjaxResult.success();
+    }
 }
